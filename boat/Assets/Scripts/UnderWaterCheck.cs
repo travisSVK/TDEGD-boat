e7dc8c3803ac5160f536a56bd38eee3c6d095a8f
Ludvig Arlebrink <ludvig.arlebrink@outlook.com>
2019-11-19 18:32:55 +0100

    Refactoring.

--- boat/Assets/Scripts/UnderWaterCheck.cs
+++ boat/Assets/Scripts/UnderWaterCheck.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class UnderWaterCheck : MonoBehaviour
 {
-    [SerializeField] private Transform[] m_ForcePoints;
-    [SerializeField] private Vector3 m_Buoyancy = new Vector3(0.0f, Physics.gravity.y, 0.0f);
-    private Water m_Water;
-    private Rigidbody m_Rigidbody;
+    [SerializeField]
+    private Transform[] m_ForcePoints = null;
+    
+    [SerializeField]
+    private Vector3 m_Buoyancy = new Vector3(0.0f, Physics.gravity.y, 0.0f);
+    
+    private Water m_Water = null;
+    private Rigidbody m_Rigidbody = null;
 
     private void Start()
     {
         m_Water = FindObjectOfType<Water>();
         m_Rigidbody = GetComponent(typeof(Rigidbody)) as Rigidbody;
     }
-
 
     private void FixedUpdate()
     {
