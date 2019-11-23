20f13ebc37dcea8c0ce6b95842394f85804b1ae6
travisko <marek.cern@gmail.com>
2019-11-23 21:51:49 +0100

    Lowered the frequency of wind particles, stabilized their noise. Added background music.

--- boat/Assets/Scripts/ParticleRotator.cs
+++ boat/Assets/Scripts/ParticleRotator.cs
@@ -14,12 +14,12 @@ public class ParticleRotator : MonoBehaviour
         int count = m_ParticleSystem.GetParticles(m_Particles);
         for (int i = 0; i < count; i++)
         {
-            if (((m_Particles[i].startLifetime - m_Particles[i].remainingLifetime) >= (m_Particles[i].startLifetime / 4.0f)) &&
-                ((m_Particles[i].startLifetime - m_Particles[i].remainingLifetime) <= (m_Particles[i].startLifetime / 4.0f * 3.0f)))
+            if (((m_Particles[i].startLifetime - m_Particles[i].remainingLifetime) >= (m_Particles[i].startLifetime / 3.0f)) &&
+                ((m_Particles[i].startLifetime - m_Particles[i].remainingLifetime) <= (m_Particles[i].startLifetime / 3.0f * 2.0f)))
             {
-                float scaleFactor = 2.0f * Mathf.PI / (m_Particles[i].startLifetime / 2.0f);
-                float cos = Mathf.Cos(scaleFactor * ((m_Particles[i].startLifetime - m_Particles[i].remainingLifetime) - (m_Particles[i].startLifetime / 4.0f)));
-                float sin = Mathf.Sin(scaleFactor * ((m_Particles[i].startLifetime - m_Particles[i].remainingLifetime) - (m_Particles[i].startLifetime / 4.0f)));
+                float scaleFactor = 2.0f * Mathf.PI / (m_Particles[i].startLifetime / 3.0f);
+                float cos = Mathf.Cos(scaleFactor * ((m_Particles[i].startLifetime - m_Particles[i].remainingLifetime) - (m_Particles[i].startLifetime / 3.0f)));
+                float sin = Mathf.Sin(scaleFactor * ((m_Particles[i].startLifetime - m_Particles[i].remainingLifetime) - (m_Particles[i].startLifetime / 3.0f)));
                 //Debug.Log("cos: " + cos + " sin: " + sin);
                 m_Particles[i].velocity = new Vector3(-10.0f * cos, 10.0f * sin, 0.0f);
             }
