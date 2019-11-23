936f22c798de2f5dcf27ee2d776ab11dbc099af8
travisko <marek.cern@gmail.com>
2019-11-23 20:58:49 +0100

    Adding cartoonish wind simulation.

---
+++ boat/Assets/Scripts/ParticleRotator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleRotator : MonoBehaviour
+{
+    private ParticleSystem m_ParticleSystem = null;
+    ParticleSystem.Particle[] m_Particles;
+
+
+    private void LateUpdate()
+    {
+        InitializeIfNeeded();
+        int count = m_ParticleSystem.GetParticles(m_Particles);
+        for (int i = 0; i < count; i++)
+        {
+            if (((m_Particles[i].startLifetime - m_Particles[i].remainingLifetime) >= (m_Particles[i].startLifetime / 4.0f)) &&
+                ((m_Particles[i].startLifetime - m_Particles[i].remainingLifetime) <= (m_Particles[i].startLifetime / 4.0f * 3.0f)))
+            {
+                float scaleFactor = 2.0f * Mathf.PI / (m_Particles[i].startLifetime / 2.0f);
+                float cos = Mathf.Cos(scaleFactor * ((m_Particles[i].startLifetime - m_Particles[i].remainingLifetime) - (m_Particles[i].startLifetime / 4.0f)));
+                float sin = Mathf.Sin(scaleFactor * ((m_Particles[i].startLifetime - m_Particles[i].remainingLifetime) - (m_Particles[i].startLifetime / 4.0f)));
+                //Debug.Log("cos: " + cos + " sin: " + sin);
+                m_Particles[i].velocity = new Vector3(-10.0f * cos, 10.0f * sin, 0.0f);
+            }
+            else
+            {
+                m_Particles[i].velocity = new Vector3(-10.0f, 0.0f, 0.0f);
+            }
+        }
+        m_ParticleSystem.SetParticles(m_Particles, count);
+    }
+
+    private void InitializeIfNeeded()
+    {
+        if (!m_ParticleSystem)
+        {
+            m_ParticleSystem = GetComponent<ParticleSystem>();
+        }
+
+        if (m_Particles == null || m_Particles.Length < m_ParticleSystem.main.maxParticles)
+        {
+            m_Particles = new ParticleSystem.Particle[m_ParticleSystem.main.maxParticles];
+        }
+    }
+}
